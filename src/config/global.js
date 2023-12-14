c8ed8a3b14d249487148fa0886b3d6bda90b68cf
emilydev <98632409+emilydev@users.noreply.github.com>
2023-12-13 21:26:37 -0500

    Se vincula pdf accesible y se genera zip.

--- src/config/global.js
+++ src/config/global.js
@@ -211,7 +211,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/223306_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
